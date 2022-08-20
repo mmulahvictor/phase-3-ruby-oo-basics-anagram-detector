39ba2fc2408e22d7200c8f8e2bbe9aa32b49bb59
mmulahvictor <mmulahvictor@gmail.com>
2022-08-20 20:36:00 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.select { |w| w.split('').sort == @word.split('').sort }
+    end
+end
